9551364b037502e52299fa432d7c20a1af60b68c
Ethan Flaharty <ethanflahartybusiness@gmail.com>
2026-05-08 14:10:28 -0500

    3. command registration functional

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Config struct {
 	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
@@ -12,4 +17,42 @@ func (c *Config) SetUser(userName string) error {
 		return err
 	}
 	return nil
+}
+
+type State struct {
+	CFG *Config
+}
+
+type Command struct {
+	Name     string
+	Username []string
+}
+
+func HandlerLogin(s *State, cmd Command) error {
+	if len(cmd.Username) < 1 {
+		return errors.New("login expects a username")
+	}
+	err := s.CFG.SetUser(cmd.Username[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println("User has been set")
+	return nil
+}
+
+type Commands struct {
+	Cmds map[string]func(*State, Command) error
+}
+
+func (c *Commands) Run(s *State, cmd Command) error {
+	handler, ok := c.Cmds[cmd.Name]
+	if !ok {
+		return errors.New("command doesn't exist")
+	}
+
+	return handler(s, cmd)
+}
+
+func (c *Commands) Register(name string, f func(*State, Command) error) {
+	c.Cmds[name] = f
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ethanflaharty/blog-aggregator/internal/config"
 )
@@ -12,15 +13,25 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
-	err = cfg.SetUser("ethan")
+	var cfgFile config.State
+	cfgFile.CFG = &cfg
+
+	var savedCmds config.Commands
+	savedCmds.Cmds = make(map[string]func(*config.State, config.Command) error)
+	savedCmds.Register("login", config.HandlerLogin)
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments provided")
+		os.Exit(1)
+	}
+	cmdName := os.Args[1]
+	cmdUsername := os.Args[2:]
+	var newCmd config.Command
+	newCmd.Name = cmdName
+	newCmd.Username = cmdUsername
+
+	err = savedCmds.Run(&cfgFile, newCmd)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		os.Exit(1)
 	}
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	fmt.Println(cfg)
 }
